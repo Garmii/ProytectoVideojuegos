fa2bbe56c4975e91e74aab65a58d61073cc66527
Garmii <mgfr.garcia.garcia@gmail.com>
2022-02-06 22:46:07 +0100

    Niveles hechos

--- Assets/_Scripts/Enemies/Cannon.cs
+++ Assets/_Scripts/Enemies/Cannon.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Cannon : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Cannon : MonoBehaviour
     public Transform firePoint;
     public Animator animator;
     private bool canShoot = true;
+    [SerializeField] private float coolDown = 2;
     
     [SerializeField] private GameObject player;
     private GameObject spawnedPlayer;
@@ -25,15 +27,15 @@ public class Cannon : MonoBehaviour
     {
         
         if (spawnedPlayer == null) return;
-        
+       
         float distance = transform.position.x - spawnedPlayer.transform.position.x;
 
-        if (canShoot == true && (distance < 10 && distance > 0))
+        if (canShoot == true && (distance < 15 && distance > 0))
         {
             animator.SetTrigger("Fire");
             Shoot();
             canShoot = false;
-            Invoke("CooldownUp", 2);
+            Invoke("CooldownUp", coolDown);
         }
     }
 
